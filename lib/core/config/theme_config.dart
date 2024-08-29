818f33a26909e24789ab393e82def05393a8f4e0
mohhamad esmaili <esmaili.mohhamad@gmail.com>
2024-08-29 15:04:41 -0700

    change theme data

--- lib/core/config/theme_config.dart
+++ lib/core/config/theme_config.dart
@@ -3,10 +3,19 @@ import 'package:mindwrite/core/utils/color_constants.dart';
 
 class ThemeConfig {
   static final darkAppTheme = ThemeData(
-    fontFamily: 'Roboto',
-    primaryColor: ColorConstants.primaryDarkColor,
-    scaffoldBackgroundColor: ColorConstants.scaffoldDarkColor,
-    appBarTheme: AppBarTheme(backgroundColor: ColorConstants.scaffoldDarkColor),
-    iconTheme: IconThemeData(color: ColorConstants.primaryDarkColor),
-  );
+      fontFamily: 'Roboto',
+      primaryColor: ColorConstants.primaryColor,
+      scaffoldBackgroundColor: ColorConstants.scaffoldDarkColor,
+      appBarTheme:
+          AppBarTheme(backgroundColor: ColorConstants.scaffoldDarkColor),
+      iconTheme: IconThemeData(color: ColorConstants.primaryDarkColor),
+      drawerTheme: DrawerThemeData(
+        backgroundColor: ColorConstants.scaffoldDarkColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(0),
+            bottomRight: Radius.circular(0),
+          ),
+        ),
+      ));
 }
