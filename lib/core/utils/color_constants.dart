a4037c6da04c1fe44fa0b91f3753bffb8ec71d7f
mohhamad esmaili <esmaili.mohhamad@gmail.com>
2024-09-16 09:29:28 -0700

    seprate note constants with primary color class

--- lib/core/utils/color_constants.dart
+++ lib/core/utils/color_constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mindwrite/features/home_feature/data/model/background_model.dart';
 
-class ColorConstants {
+class AppColorConstants {
   /// app colors
   static Color scaffoldDarkColor = const Color.fromRGBO(19, 19, 19, 1);
   static Color appbarDarkColor = const Color.fromRGBO(30, 31, 33, 1);
@@ -18,17 +18,5 @@ class ColorConstants {
   /// editional colors
   static Color drawerDividerColor = const Color.fromRGBO(23, 64, 116, 1);
   static Color snackBarWhitecolor = const Color.fromRGBO(227, 227, 227, 1);
-  static List<BackgroundModel> noteColors = [
-    BackgroundModel(color: Colors.grey[800]!),
-    BackgroundModel(color: const Color.fromRGBO(105, 42, 24, 1)),
-    BackgroundModel(color: const Color.fromRGBO(124, 74, 3, 1)),
-    BackgroundModel(color: const Color.fromRGBO(38, 77, 59, 1)),
-    BackgroundModel(color: const Color.fromRGBO(12, 99, 93, 1)),
-    BackgroundModel(color: const Color.fromRGBO(37, 100, 118, 1)),
-    BackgroundModel(color: const Color.fromRGBO(39, 66, 85, 1)),
-    BackgroundModel(color: const Color.fromRGBO(72, 46, 91, 1)),
-    BackgroundModel(color: const Color.fromRGBO(109, 57, 79, 1)),
-    BackgroundModel(color: const Color.fromRGBO(75, 68, 58, 1)),
-    BackgroundModel(color: const Color.fromRGBO(35, 36, 40, 1)),
-  ];
+  static Color noteSelectionColor = const Color.fromRGBO(168, 200, 249, 1);
 }
---
+++ lib/core/utils/note_constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mindwrite/core/gen/assets.gen.dart';
+import 'package:mindwrite/features/home_feature/data/model/background_model.dart';
+
+class NoteConstants {
+  static List<BackgroundModel> noteColors = [
+    BackgroundModel(color: const Color.fromRGBO(19, 19, 19, 1)),
+    BackgroundModel(color: const Color.fromRGBO(105, 42, 24, 1)),
+    BackgroundModel(color: const Color.fromRGBO(124, 74, 3, 1)),
+    BackgroundModel(color: const Color.fromRGBO(38, 77, 59, 1)),
+    BackgroundModel(color: const Color.fromRGBO(12, 99, 93, 1)),
+    BackgroundModel(color: const Color.fromRGBO(37, 100, 118, 1)),
+    BackgroundModel(color: const Color.fromRGBO(39, 66, 85, 1)),
+    BackgroundModel(color: const Color.fromRGBO(72, 46, 91, 1)),
+    BackgroundModel(color: const Color.fromRGBO(109, 57, 79, 1)),
+    BackgroundModel(color: const Color.fromRGBO(75, 68, 58, 1)),
+    BackgroundModel(color: const Color.fromRGBO(35, 36, 40, 1)),
+  ];
+  static List<BackgroundModel> noteDarkBackgrounds = [
+    BackgroundModel(backgroundPath: null),
+    BackgroundModel(backgroundPath: Assets.images.foodNight.path),
+    BackgroundModel(backgroundPath: Assets.images.fruitNight.path),
+    BackgroundModel(backgroundPath: Assets.images.poolNight.path),
+    BackgroundModel(backgroundPath: Assets.images.tranNight.path),
+    BackgroundModel(backgroundPath: Assets.images.parkNight.path),
+  ];
+}
